c0a8ac4ce98edeba549db8aa4688555817d1618a
danielh13210 <danielh13210@gmail.com>
2026-06-14 02:53:09 +0000

    Check file type too

--- run.py
+++ run.py
@@ -16,3 +16,25 @@ try:
   success("build successful")
 except CalledProcessError:
   fail("cannot build",fatal=True)
+if os.path.isfile('spawn_sim'):
+  success("spawn_sim found")
+else:
+  fail("spawn_sim not found",fatal=True)
+if os.access('spawn_sim',os.X_OK):
+  success("spawn_sim is an executable file")
+else:
+  fail("spawn_sim is not executable",fatal=True)
+try:
+  out=subprocess.check_output(['readelf', '-h', 'spawn_sim'], text=True)
+  out=out.splitlines()
+  out=[[ part.strip() for part in line.strip().split(':')] for line in out] # strip and split
+  for line in out:
+    if line[0]=="Machine" and line[1]=="AArch64":
+      success("AArch64 executable detected")
+      aarch=True
+      break
+  if not aarch:
+    fail("Executable not AArch64",fatal=True)
+except CalledProcessError:
+  fail("spawn_sim is not an ELF",fatal=True)
+
